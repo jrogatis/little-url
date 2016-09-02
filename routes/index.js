d7cf238492739e57b35ddad93b178ecdadc0e5c7
JEAN P DE ROGATIS <jrogatis@conecta.net.br>
2016-09-02 18:20:12 -0300

    o ywahh

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,44 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const pg = require('pg');
+const DATABASE_URL = 'postgres://fidbvttodbpssc:ygSkoG5ECKgTGVI_iTlB-MD2rQ@ec2-54-243-28-22.compute-1.amazonaws.com:5432/d6vqln1g3antb3'
+var clientRet;
 
-/* GET home page. */
+pg.defaults.ssl = true;
 
-router.get('/', function(req, res) {
+pg.connect(DATABASE_URL, (err, client) => {
+    if (err) throw err;
+    console.log('Connected to postgres! Getting tables...');
+    clientRet = client;
+});
+
+
+router.get('/', (req, res) =>{
   res.render('index', { title: 'Express' });
+});
+
+
+
+router.get('/new/:longUrl',(req, res)=> {
+    const fullUrl = req.get('host');
+    const longUrl = req.params.longUrl;
+   //console.log(clientRet)
+    clientRet
+        .query(`SELECT
+                    "Index",
+                    "URL"
+                FROM
+                    public."urlsToLitleUrl"
+                WHERE
+                   "URL" = '${longUrl}';`)
+        .on('row', row =>{
+                console.log('dentro do check',row);
+                res.end(`{ "original_url":"${longUrl}", "short_url":"${fullUrl}/${row.Index}" }`);
+                }
+
+           );
+
+
 });
 
 module.exports = router;
