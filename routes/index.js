c1d2a51da9d2ea6bddd9ae3203bf6f0118c81c25
JEAN P DE ROGATIS <jrogatis@conecta.net.br>
2016-09-02 22:57:14 -0300

    Final! with url check

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const pg = require('pg');
+const validator = require('validator');
 const DATABASE_URL = 'postgres://fidbvttodbpssc:ygSkoG5ECKgTGVI_iTlB-MD2rQ@ec2-54-243-28-22.compute-1.amazonaws.com:5432/d6vqln1g3antb3'
-var clientRet;
+
 
 pg.defaults.ssl = true;
 
@@ -10,15 +11,19 @@ pg.connect(DATABASE_URL, (err, client) => {
     if (err) throw err;
     console.log('Connected to postgres! Getting tables...');
 
-    router.get('/', (req, res) =>{
-  res.render('index', { title: 'Express' });
-});
-
-     router.get('/new', (req, res) =>{
-        res.render('index', { title: 'Express' });
+    router.get('/', (req, res) => {
+        res.render('index', {
+            title: 'Express'
+        });
     });
 
-    router.get('/:index', (req, res) =>{
+    router.get('/new', (req, res) => {
+        res.render('index', {
+            title: 'Express'
+        });
+    });
+
+    router.get('/:index', (req, res) => {
         client
             .query(`SELECT
                         "Index",
@@ -27,37 +32,79 @@ pg.connect(DATABASE_URL, (err, client) => {
                         public."urlsToLitleUrl"
                     WHERE
                        "Index" = ${req.params.index};`)
-            .on('row', row =>{
-                    console.log('dentro do redirect',row.URL);
+            .on('row', row => {
+                    console.log('dentro do redirect', row.URL);
                     res.redirect(row.URL);
-                    }
+                }
 
-               );
+            );
 
 
     });
 
-
-
-    router.get('/new/:longUrl',(req, res)=> {
+    router.get('/new/*', (req, res) => {
+        console.log('passeisim',req.params[0]);
         const fullUrl = req.get('host');
-        const longUrl = req.params.longUrl;
-        client
-            .query(`SELECT
-                        "Index",
-                        "URL"
-                    FROM
-                        public."urlsToLitleUrl"
-                    WHERE
-                       "URL" = '${longUrl}';`)
-            .on('row', row =>{
+        const longUrl = req.params[0];
+        let urls = [];
+        console.log(validator.isURL(longUrl, {require_protocol: true}));
+        if (!validator.isURL(longUrl, {require_protocol: true})) {
+            res.end('its not a valid Url')
+        } else {
+            console.log('saco');
 
-                    console.log('dentro do check',row);
-                   res.end(`{ "original_url":"${longUrl}", "short_url":"${fullUrl}/${row.Index}" }`);
+            client
+                .query(`SELECT
+                            "Index",
+                            "URL"
+                        FROM
+                            public."urlsToLitleUrl"
+                        WHERE
+                           "URL" = '${longUrl}';`
+                 )
+                .on('row', row => {
+
+                        console.log('dentro do check', row);
+
+                       urls.push(row);
+
+                })
+                .on('end', () => {
+                    console.log(urls.length)
+                    if (urls.length > 0) {
+                        res.writeHead(200, {
+                                'content-type': 'text/plain'
+                            });
+                        res.end(`{ "original_url":"${longUrl}", "short_url":"${fullUrl}/${urls[0].Index}" }`);
+                    } else {
+                        urls=[]
+                        client
+                            .query(`INSERT INTO  public."urlsToLitleUrl" ("URL") VALUES ('${ req.params[0]}');`)
+                            .on('end', () => {
+                                client
+                                    .query(`SELECT
+                                                "Index",
+                                                "URL"
+                                            FROM
+                                                public."urlsToLitleUrl"
+                                            WHERE
+                                               "URL" = '${longUrl}';`
+                                      )
+                                    .on('row', row => {
+
+                                            console.log('dentro do check', row);
+                                            urls.push(row);
+
+                                            })
+                                    .on('end', ()=>{
+                                              res.end(`{ "original_url":"${longUrl}", "short_url":"${fullUrl}/${urls[0].Index}" }`);
+                                        })
+                            })
+
                     }
+                })
 
-               );
-
+        }
 
     });
 
